db0499058aaf51fed80a99dd7cc3dd0bcc5d1c65
DanielEgidiio <danielegidioti@gmail.com>
2022-02-23 10:15:41 -0300

    Setup da sessão About

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
+
 import "./About.scss";
 
+const abouts = [
+  {
+    title: "Desenvolvedor Web",
+    description: "Eu sou um bom desenvolvedor Web.",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Web Design",
+    description: "Eu sou um bom Web Design.",
+    imgUrl: images.about02,
+  },
+  {
+    title: "UI/UX",
+    description: "Eu sou um bom desenvolvedor Web.",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Transformação Digital",
+    description: "Eu sou um bom desenvolvedor Web.",
+    imgUrl: images.about04,
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text" style={{ fontSize: 35 }}>
+        "O segredo do sucesso é fazer coisas comuns <br />
+        de maneira incomumente bem". <span>John D. Rockefeller Jr</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
