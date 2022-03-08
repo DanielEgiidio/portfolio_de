1024aefe99d1dd9145fbbe7662543e547230ecbf
DanielEgidiio <danielegidioti@gmail.com>
2022-03-08 12:05:55 -0300

    Finalização do footer e projeto

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.scss";
 
 import { images } from "../../constants";
@@ -6,7 +6,118 @@ import { AppWrap, MotionWrap } from "../../wrapper";
 import { client } from "../../client";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmited, setIsFormSubmited] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { username, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmited(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Entre em Contato</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a
+            href="mailto:danielegidioti@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="p-text"
+          >
+            danielegidioti@gmail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a
+            href="https://wa.me/5583999083810?text=Como+posso+Ajudar%3F"
+            target="_blank"
+            rel="noreferrer"
+            className="p-text"
+          >
+            +55 (83) 99908-3810
+          </a>
+        </div>
+      </div>
+      {!isFormSubmited ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              required
+              placeholder="Seu Nome"
+              name="username"
+              value={username}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              required
+              placeholder="Seu Email"
+              name="email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              required
+              placeholder="Sua Mensagem"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {!loading ? "Enviar Mensagem" : "Enviando..."}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">Obrigado por entrar em contato!</h3>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default Footer;
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contato",
+  "app__whitebg"
+);
